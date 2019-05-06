e2bf71fe6090a83e5126ecdafd1ad2b2fb4999bd
Daniel <danypaez142@gmail.com>
2019-05-06 18:10:07 -0300

    Se añadió el proyecto de Aeropuertos

---
+++ Aeropuerto/src/test/java/ar/edu/undec/AerolineaUnitTest.java
@@ -0,0 +1,13 @@
+package ar.edu.undec;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AerolineaUnitTest {
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}	
+}
+
---
+++ Aeropuerto/src/test/java/ar/edu/undec/PilotoUnitTest.java
@@ -0,0 +1,14 @@
+package ar.edu.undec;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PilotoUnitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
